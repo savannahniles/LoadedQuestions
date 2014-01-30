89c428c09f67bf264217a6b9eb6334968e87a4eb
Savannah Niles <savannahniles@gmail.com>
2014-01-29 21:26:45 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoadedQuestions::Application.config.secret_key_base = '8d7d3a34d51141f1ffc3c06bcbc870d51861fd5cf51d0648c1b15e4de581aab095a37f63f22ab55cf6576704e9a55d3505181477adafc5be605af28f6f906862'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoadedQuestions::Application.config.session_store :cookie_store, key: '_LoadedQuestions_session'
